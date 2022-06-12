d6a3f1d3ae712876795222fea2caf010076bcab4
pkosecki <koseckipaw@gmail.com>
2022-06-12 16:12:43 +0200

    1.0
    1.0

---
+++ RPN/RPN/RPN.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RPNCalulator {
+	public class RPN {
+		private Stack<int> _operators;
+		Dictionary<string, Func<int, int, int>> _operationFunction;
+
+		
+
+		public int EvalRPN(string input) {
+			_operationFunction = new Dictionary<string, Func<int, int, int>>
+			{
+				["+"] = (fst, snd) => (fst + snd),
+				["-"] = (fst, snd) => (fst - snd),
+				["*"] = (fst, snd) => (fst * snd),
+				["/"] = (fst, snd) => Dzielenie(fst, snd),
+				["!"] = (fst, snd) => Factorial(fst)
+				
+			};
+			
+			_operators = new Stack<int>();
+
+			var splitInput = input.Split(' ');
+			foreach (var op in splitInput)
+			{
+				if (IsNumber(op))
+					_operators.Push(Int32.Parse(op));
+				else
+				if (IsOperator(op))
+				{
+					var num1 = _operators.Pop();
+					var num2 = _operators.Pop();
+					_operators.Push(_operationFunction[op](num1, num2));
+					//_operators.Push(Operation(op)(num1, num2));
+				}
+				else
+					if(IsOneArgumentOperator(op))
+				{
+					var num1 = _operators.Pop();
+					_operators.Push(_operationFunction[op](num1, 0));
+				}
+				
+			}
+
+			var result = _operators.Pop();
+			if (_operators.IsEmpty)
+			{
+				return result;
+			}
+			throw new InvalidOperationException();
+		}
+
+		private int Dzielenie(int fst, int snd)
+		{
+			try
+			{
+				return(fst / snd);
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("Division of {0} by zero.", fst);
+				return 999999999;
+			}
+		}
+
+		private int Factorial(int fst)
+		{
+			return fst < 1 ? 1 : fst * Factorial(fst - 1);
+		}
+
+		private bool IsNumber(String input) => Int32.TryParse(input, out _);
+
+		private bool IsOperator(String input) =>
+			input.Equals("+") || input.Equals("-") ||
+			input.Equals("*") || input.Equals("/");
+
+		private bool IsOneArgumentOperator(String input) =>
+			input.Equals("!");
+
+		private Func<int, int, int> Operation(String input) =>
+			(x, y) =>
+			(
+				(input.Equals("+") ? x + y :
+					(input.Equals("*") ? x * y : int.MinValue)
+				)
+			);
+	}
+}
---
+++ RPNTest/RPNTest.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using System;
+using RPNCalulator;
+
+namespace RPNTest {
+	[TestFixture]
+	public class RPNTest {
+		private RPN _sut;
+		[SetUp]
+		public void Setup() {
+			_sut = new RPN();
+		}
+		[Test]
+		public void CheckIfTestWorks() {
+			Assert.Pass();
+		}
+
+		[Test]
+		public void CheckIfCanCreateSut() {
+			Assert.That(_sut, Is.Not.Null);
+		}
+
+		[Test]
+		public void SingleDigitOneInputOneReturn() {
+			var result = _sut.EvalRPN("1");
+
+			Assert.That(result, Is.EqualTo(1));
+
+		}
+		[Test]
+		public void SingleDigitOtherThenOneInputNumberReturn() {
+			var result = _sut.EvalRPN("2");
+
+			Assert.That(result, Is.EqualTo(2));
+
+		}
+		[Test]
+		public void TwoDigitsNumberInputNumberReturn() {
+			var result = _sut.EvalRPN("12");
+
+			Assert.That(result, Is.EqualTo(12));
+
+		}
+		[Test]
+		public void TwoNumbersGivenWithoutOperator_ThrowsExcepton() {
+			Assert.Throws<InvalidOperationException>(() => _sut.EvalRPN("1 2"));
+
+		}
+		[Test]
+		public void OperatorPlus_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("1 2 +");
+
+			Assert.That(result, Is.EqualTo(3));
+		}
+		[Test]
+		public void OperatorTimes_AddingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("2 2 *");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+		[Test]
+		public void OperatorMinus_SubstractingTwoNumbers_ReturnCorrectResult() {
+			var result = _sut.EvalRPN("1 2 -");
+
+			Assert.That(result, Is.EqualTo(1));
+		}
+		[Test]
+		public void ComplexExpression() {
+			var result = _sut.EvalRPN("15 7 1 1 + - / 3 * 2 1 1 + + -");
+
+			Assert.That(result, Is.EqualTo(4));
+		}
+		[Test]
+		public void Factorial()
+		{
+			var result = _sut.EvalRPN("3 !");
+
+			Assert.That(result, Is.EqualTo(6));
+		}
+		[Test]
+		public void ComplicatedWithFactorial()
+		{
+			var result = _sut.EvalRPN("2 2 2 + ! +");
+			Assert.That(result, Is.EqualTo(26));
+		}
+		[Test]
+		public void Partition()
+		{
+			var result = _sut.EvalRPN("2 10 /");
+			Assert.That(result, Is.EqualTo(5));
+		}
+		[Test]
+		public void Partition2()
+		{
+			var result = _sut.EvalRPN("0 2 /");
+			Assert.That(result, Is.EqualTo(999999999));
+		}
+	}
+}
